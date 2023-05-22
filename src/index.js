35bd341baac874285f0f64ca13b398cc85044bbb
sun8282 <109707901+sun8282@users.noreply.github.com>
2023-05-22 20:35:36 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Squads from "./components/TeamSquad";
+import Playerdetails from "./components/playerdetails";
 import "./index.css";
 import "./reset.css";
 import App from "./App";
@@ -19,6 +20,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/details/:id" element={<Squads />} />
+      <Route path="/playerdetails/:id" element={<Playerdetails />} />
     </Routes>
   </BrowserRouter>
 );
