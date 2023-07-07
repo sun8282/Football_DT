2af5990b3925a83145430598fa40b34b2d51c7d0
sun8282 <109707901+sun8282@users.noreply.github.com>
2023-07-07 21:00:05 +0900

    Add files via upload

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,6 +5,7 @@ function Quiz() {
     // const jsonData = await response.json();
     const [QuizData, setQuizData] = useState([]);
     const [EnKr, setleng] = useState(true);
+    const [answer,setanswer] = useState([]);
     const Exambox = styled.div`
                     width: 100px; 
                     height: 100px; 
@@ -21,8 +22,10 @@ function Quiz() {
                 console.log(result)
                 if(EnKr === true && result.kr && result.kr.length >0){
                     setQuizData(result.kr);
+                    setanswer(result.answer)
                 }else if(EnKr === false && result.en && result.en.length >0){
                     setQuizData(result.en);
+                    setanswer(result.answer)
                 }
             // }
         //   } catch (error) {
@@ -77,7 +80,70 @@ function Quiz() {
             exams[1].classList.remove("opa")
             exams[2].classList.add("opa")
         }
+    }
+    function hintClick(el){
+        const hint = el.target.nextSibling;
+        hint.classList.remove("none");
+    }
+    function ClickNext(el){
+        const exams = document.querySelectorAll(".exambox")
+        const btns = document.querySelectorAll(".next")
+        const step1 = document.querySelector(".step1 label h2");
+        const step2 = document.querySelector(".step2 label h2");
+        const step3 = document.querySelector(".step3 label h2");
+        console.log(exams);
+        if(el.target == btns[0]){
+            exams[0].classList.remove("opa")
+            exams[1].classList.add("opa")
+            exams[2].classList.remove("opa")
+            step1.classList.remove("act")
+            step2.classList.add("act")
+            step3.classList.remove("act")
+
+        }else{
+            exams[0].classList.remove("opa")
+            exams[1].classList.remove("opa")
+            exams[2].classList.add("opa")
+            step1.classList.remove("act")
+            step2.classList.remove("act")
+            step3.classList.add("act")
+        }
+    }
+    function answerSubmit(){
+        const options = document.querySelectorAll(".option label input")
+        const answerinput = document.querySelectorAll(".answerinput");
+        console.log(answerinput);
         
+        let Useranswer =[]
+        options.forEach(element => {
+        if(element.checked == true){
+            Useranswer.push([element.className,element.value])
+        }    
+        
+        
+        });
+        answerinput.forEach(e =>{
+            console.log(e.value);
+            if(e.value !==""){
+            Useranswer.push([e.value,e.name]);
+        }
+        })
+        console.log(Useranswer,answer);
+        Useranswer.forEach((e,idx) =>{
+            
+            if(idx<8){
+                if(answer[idx].includes(e[0])){
+                    console.log(e);
+                }
+            }else{
+                answer[idx].forEach(v =>{
+                    console.log(v,e);
+                    if(v == e[0]){
+                        console.log(e);
+                    }
+                })
+            }
+        })
     }
     if(QuizData.length >0){
     return (
@@ -113,25 +179,29 @@ function Quiz() {
                             </div>
                             <div className="options">
                                 {element.options.map((e,idx) =>{
-                                    return(
-                                        <div className="option">
-                                            <label for={i+"option"+idx}>
-                                                <input type="radio" id={i+"option"+idx} name={"question"+i} value={element.point}></input>
-                                                {e}
-                                            </label>
-                                        </div>
+                                    return(                                    
+                                            <div className="option">
+                                                <label for={i+"option"+idx}>
+                                                    <input type="radio" id={i+"option"+idx} name={"question"+i} value={element.point} className={idx+1}></input>
+                                                    <span>{e}</span>
+                                                </label>
+                                            </div>                                    
                                     )
                                 })}
+                                <div className="hintbox">
+                                    <button onClick={hintClick}>힌트 보기</button>
+                                    <h5 className="none">힌트 : {element.hint}</h5>
+                                </div>
 
                             </div>
                         </div>
                         </>
                         )
                     })}
-                    <button className="next">Next</button>
+                    <button className="next" onClick={ClickNext}>Next</button>
                 </div>
                 <div className="exambox " >
-                    {QuizData[1].step2[0].map(element => {
+                    {QuizData[1].step2[0].map((element,idx) => {
                       console.log(element.img);
                       return(
                         <>
@@ -139,27 +209,39 @@ function Quiz() {
                             <div className="questiontitle">
                                 <h3>{element.title}</h3>
                                 <h4>point: {element.point}</h4>
-                                <img src={element.img}></img>
-                                <input type="text" placeholder="선수 이름을 입력하세요."></input>
+                                {/* <img src={element.img}></img> */}
+                                <div className={"img"+idx}></div>
+                                <input type="text" placeholder="선수 이름을 입력하세요." className="answerinput" name={element.point}></input>
+                                <div className="hintbox">
+                                    <button onClick={hintClick}>힌트 보기</button>
+                                    <h5 className="none">힌트 : {element.hint}</h5>
+                                </div>
                             </div>
                         </div> 
                         </>
                         )
                     })}
-                    <button className="next">Next</button>
+                    <button className="next" onClick={ClickNext}>Next</button>
                 </div>
                 <div className="exambox " >
-                    {QuizData[2].step3[0].map(element => {
+                    {QuizData[2].step3[0].map((element,idx) => {
                       console.log(element.title);
                       return(
                         <>
                         <div className="questionbox">
-                            <div>{element.title}</div>
+                            <h3>{element.title}</h3>
+                            <h4>point: {element.point}</h4>
+                            <div className={"teamimg"+idx}></div>
+                            <input type="text" placeholder="팀 이름을 입력하세요." className="answerinput" name={element.point}></input>
+                            <div className="hintbox">
+                                    <button onClick={hintClick}>힌트 보기</button>
+                                    <h5 className="none">힌트 : {element.hint}</h5>
+                                </div>
                         </div> 
                         </>
                         )
                     })}
-                    <button className="done">Done</button>
+                    <button className="done" onClick={answerSubmit}>Done</button>
                 </div>
             </div>
         </>
